c12969ddbe462a2e296cd70dda7705fb4378067f
Mr-G-D <daredinesh00@gmail.com>
2021-09-15 21:55:47 +0530

    static sorting bar with randomly generated values

--- src/components/SortingBars.jsx
+++ src/components/SortingBars.jsx
@@ -1,23 +1,29 @@
 import React from "react";
 import { makeStyles } from "@material-ui/styles";
 
-let Array = [200, 300, 400, 500];
-let bars = Array.map((bar) => (
-  <div
-    style={{
-      height: bar,
-      backgroundColor: "#ff0000",
-      width: 5,
-      marginRight: 1,
-    }}
-  ></div>
-));
+let array = [];
+for (let i = 0; i < 200; i++) {
+  array.push(Math.random() * 500 + 10);
+}
 
 const SortingBars = () => {
   const styles = useStyles();
   return (
     <div className={styles.main}>
-      <div className={styles.body}>{bars}</div>
+      <div className={styles.body}>
+        {array.map((bar) => (
+          <div
+            key={bar}
+            style={{
+              height: bar,
+              backgroundColor: "#ff0000",
+              width: 5,
+              marginRight: 1,
+              borderRadius: "50px",
+            }}
+          ></div>
+        ))}
+      </div>
 
       <hr className={styles.hr} />
     </div>
@@ -37,6 +43,7 @@ const useStyles = makeStyles({
     marginBottom: "0px",
     display: "flex",
     alignItems: "flex-end",
+    justifyContent: "center",
   },
   hr: {
     margin: "0px",
