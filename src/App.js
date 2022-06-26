bf1487bb96f522e851091c32a9cc4f3ea20d5cf8
Mr-G-D <daredinesh00@gmail.com>
2022-06-26 11:00:47 +0530

    :wrench: generate array

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Appbar from "./components/AppBar";
+import { generateArray } from "./components/Helper/helper";
+import { InsertionSort } from "./components/SortingAlgorithms/InsertionSort";
 import SortingBars from "./components/SortingBars";
 
 function App() {
+  const [array, setArray] = useState(generateArray(50));
+
+  useEffect(() => {
+    setArray(generateArray(50));
+  }, []);
+
+  const sortArray = async (array) => {
+    InsertionSort(array, setArray);
+  };
   return (
     <div className="app">
-      <Appbar />
-      <SortingBars />
+      <Appbar function={sortArray} array={array} />
+      <SortingBars array={array} />
     </div>
   );
 }
--- src/components/Helper/helper.js
+++ src/components/Helper/helper.js
@@ -15,3 +15,13 @@ export const dehighlight = (a, b) => {
   document.getElementById(a).style.backgroundColor = "dodgerBlue";
   document.getElementById(b).style.backgroundColor = "dodgerBlue";
 };
+
+export const generateArray = (len) => {
+  let array = [];
+  const min = 100,
+    max = 600;
+  for (let index = 0; index < len; index++) {
+    array.push(Math.floor(Math.random() * (max - min) + min));
+  }
+  return array;
+};
